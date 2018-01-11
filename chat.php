88612e116e2d0394677fcc6cdc4419f3077b392e
sudani12 <shreyansudani@gmail.com>
2018-01-11 00:03:36 -0600

    Update chat.php

--- chat.php
+++ chat.php
@@ -25,4 +25,13 @@ $(document).ready(function(){
 </script>
 <?php
 }
+
+session_start();
+if(isset($_SESSION['name'])){
+    $text = $_POST['text'];
+     
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
+    fclose($fp);
+}
 ?>
